e5fed667a1d7d779fe7ed8974793eed4c00bd727
Kuldeep Ranjan <ranjankuldeep19@gmail.com>
2022-12-27 15:34:23 +0530

    Got acess to peer 1 camera

---
+++ main.js
@@ -0,0 +1,11 @@
+let localStream;
+let remoteStream;
+
+let init = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById("user-1").srcObject = localStream;
+};
+init();
